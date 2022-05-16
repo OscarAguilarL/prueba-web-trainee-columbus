ad38d629a0342244a6eaca504f7a172dcd464cec
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-05-16 00:06:53 -0500

    Add form submit handler using useForm hook

--- src/components/Beneficiary.tsx
+++ src/components/Beneficiary.tsx
@@ -1,13 +1,37 @@
-import { Button } from "./Button"
+import { FormEvent } from 'react'
+import { useForm } from '../hooks'
+import { Button } from './Button'
 
 export const Beneficiary = () => {
+  const {
+    onInputChange,
+    onResetForm,
+    fullname,
+    relationship,
+    percentage,
+    CURP,
+    contactphone,
+  } = useForm({
+    fullname: '',
+    relationship: '',
+    percentage: '',
+    CURP: '',
+    contactphone: '',
+  })
+
+  const handleSubmitFn = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log(fullname, relationship, percentage, CURP, contactphone)
+    onResetForm()
+  }
+
   return (
     <div className="beneficiary">
       <h2 className="beneficiary__title">
         Añadir <br /> beneficiarios
       </h2>
 
-      <form className="beneficiary__form">
+      <form className="beneficiary__form" onSubmit={handleSubmitFn}>
         <p className="beneficiary__form--title">
           Recuerda que puedes añadir hasta cinco beneficiarios:
         </p>
@@ -15,33 +39,68 @@ export const Beneficiary = () => {
         <div className="beneficiary__form-first">
           <div className="beneficiary__form--field">
             <label htmlFor="fullname">Beneficiario 1</label>
-            <input type="text" id="fullname" placeholder="Nombre completo" />
+            <input
+              type="text"
+              name="fullname"
+              id="fullname"
+              placeholder="Nombre completo"
+              value={fullname}
+              onChange={onInputChange}
+            />
           </div>
           <div className="beneficiary__form--field">
             <label htmlFor="relationship">Parentesco</label>
-            <input type="text" id="relationship" placeholder="Conyugue" />
+            <input
+              type="text"
+              name="relationship"
+              id="relationship"
+              placeholder="Conyugue"
+              value={relationship}
+              onChange={onInputChange}
+            />
           </div>
           <div className="beneficiary__form--field">
             <label htmlFor="percentage">Porcentaje</label>
-            <input type="text" id="percentage" placeholder="100%" />
+            <input
+              type="text"
+              name="percentage"
+              id="percentage"
+              placeholder="100%"
+              value={percentage}
+              onChange={onInputChange}
+            />
           </div>
         </div>
 
         <div className="beneficiary__form-second">
           <div className="beneficiary__form--field">
             <label htmlFor="CURP">CURP</label>
-            <input type="text" id="CURP" placeholder="ABCD 000000 ABCDEFG 00" />
+            <input
+              type="text"
+              name="CURP"
+              id="CURP"
+              placeholder="ABCD 000000 ABCDEFG 00"
+              value={CURP}
+              onChange={onInputChange}
+            />
           </div>
           <div className="beneficiary__form--field">
             <label htmlFor="contactphone">Teléfono de contacto</label>
-            <input type="tel" id="contactphone" placeholder="0000000000" />
+            <input
+              type="tel"
+              name="contactphone"
+              id="contactphone"
+              placeholder="0000000000"
+              value={contactphone}
+              onChange={onInputChange}
+            />
           </div>
           <div className="beneficiary__form--field">
             <Button className="beneficiary__form--button">Remover</Button>
           </div>
         </div>
         <div className="beneficiary__form-third">
-          <Button isPrimary className="beneficiary__form--submit">
+          <Button type="submit" className="beneficiary__form--submit">
             Añadir beneficiario
           </Button>
         </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,15 +3,18 @@ import { FC, PropsWithChildren } from 'react'
 interface ButtonProps extends PropsWithChildren<unknown> {
   className?: string
   isPrimary?: boolean
+  type?: 'button' | 'submit' | 'reset' | undefined
 }
 
 export const Button: FC<ButtonProps> = ({
   className,
   children,
   isPrimary = false,
+  type = 'button',
 }) => {
   return (
     <button
+      type={type}
       className={`app__button ${
         isPrimary ? 'is-primary' : 'is-secondary'
       } ${className}`}
